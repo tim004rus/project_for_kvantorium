72a1ee597eca603c2f121020e43e978b6d275725
Eloquencer <63792546+Eloquencer@users.noreply.github.com>
2020-04-27 20:04:07 +0500

    Update project.py

--- project.py
+++ project.py
@@ -14,13 +14,13 @@ lbl.grid(column=0, row=0)
 
 
 #Radio Buttons
-rad1 = Radiobutton(window, text="название категории1", value=1)
+rad1 = Radiobutton(window, text="Доклад/сочинение", value=1)
 rad1.grid(column=0, row=3)
-rad2 = Radiobutton(window, text="название категории2", value=2)
+rad2 = Radiobutton(window, text="Творческая работа", value=2)
 rad2.grid(column=0, row=4)
-rad3 = Radiobutton(window, text="название категории3", value=3)
+rad3 = Radiobutton(window, text="Каталог/сборник", value=3)
 rad3.grid(column=0, row=5)
-rad4 = Radiobutton(window, text="название категории4", value=4)
+rad4 = Radiobutton(window, text="Другое", value=4)
 rad4.grid(column=0, row=6)
 
 window.mainloop()
