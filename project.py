bc980b60c9a1f656d9b6ff6da652a1d49397136e
tim004rus <63792189+tim004rus@users.noreply.github.com>
2020-05-01 13:33:20 +0500

    01.05.2020
    Was update Messagebox

--- project.py
+++ project.py
@@ -1,3 +1,5 @@
+# from & import
+import random
 from tkinter import *
 from tkinter import messagebox
 from tkinter.ttk import Radiobutton
@@ -6,20 +8,141 @@ from tkinter.ttk import Radiobutton
 #Program name
 window = Tk()
 window.title("Названия для файлов")
-window.geometry('250x250')
+window.geometry('400x250')
 
 
 # MessageBox
-def a():
-	messagebox.askquestion('Доклад/сочинение', 'Название файла')
-def a():
-	messagebox.showinfo('Доклад/сочинение', 'Название файла')
-def b():
-	messagebox.showinfo('Творческая работа', 'Название файла')
-def c():
-	messagebox.showinfo('Каталог/сборник', 'Название файла')
-def d():
-	messagebox.showinfo('Другое', 'Название файла')
+def rad1():
+	window = Tk()
+	window.title("Доклад/сочинение")
+	window.geometry('400x250')
+
+	def clicked():
+		probability = random.randint(1, 4)
+
+		if probability == 1:
+			res = "Доклад про {}.\nТеперь и с перцем.".format(txt.get())
+			lbl.configure(text=res)
+		if probability == 2:
+			res = "Сочинение о {}.\nВозможна поломка мозгов.".format(txt.get())
+			lbl.configure(text=res)
+		if probability ==3:
+			res = "Доклад о {}.\nНе открывать до 30.07.30.".format(txt.get())
+			lbl.configure(text=res)
+		if probability == 4:
+			res = "Сочинение о {}.\nВсе права защищены.".format(txt.get())
+			lbl.configure(text=res)
+
+
+	lbl = Label(window, text='Введите общую тему в предложном падеже')
+	lbl.grid(column=0, row=0)
+
+	txt = Entry(window, width=20)
+	txt.grid(column=0, row=1)
+
+	btn = Button(window, text="Клик!", command=clicked)
+	btn.grid(column=1, row=1)
+
+	window.mainloop()
+
+
+def rad2():
+	window = Tk()
+	window.title("Творческая работа")
+	window.geometry('400x250')
+
+	def clicked():
+		probability = random.randint(1, 4)
+		
+		if probability == 1:
+			res = "Творческая работа по теме {}.\nКто нашёл тот здохнет".format(txt.get())
+			lbl.configure(text=res)
+		if probability == 2:
+			res = "Творческая работа по теме {}.\nС любовью из психушки.".format(txt.get())
+			lbl.configure(text=res)
+		if probability ==3:
+			res = "Творческая работа по теме {}.\nДальтоникам посвящается...".format(txt.get())
+			lbl.configure(text=res)
+		if probability == 4:
+			res = "Творческая работа по теме {}.\nПотом сделаю, честно".format(txt.get())
+			lbl.configure(text=res)
+
+	lbl = Label(window, text='Введите общую тему в предложном падеже')
+	lbl.grid(column=0, row=0)
+
+	txt = Entry(window, width=20)
+	txt.grid(column=0, row=1)
+
+	btn = Button(window, text="Клик!", command=clicked)
+	btn.grid(column=1, row=1)
+
+	window.mainloop()
+
+
+def rad3():
+	window = Tk()
+	window.title("Каталог/сборник")
+	window.geometry('400x250')
+
+	def clicked():
+		probability = random.randint(1, 4)
+		
+		if probability == 1:
+			res = "Каталог {}.\nОн точно был где-то тут.".format(txt.get())
+			lbl.configure(text=res)
+		if probability == 2:
+			res = "Сборник {}.\nМы проверили, всё чисто.".format(txt.get())
+			lbl.configure(text=res)
+		if probability ==3:
+			res = "Каталог {}.\nКажется, вирусняк.".format(txt.get())
+			lbl.configure(text=res)
+		if probability == 4:
+			res = "Сборник {}.\nАбсолютно бесполезен.".format(txt.get())
+			lbl.configure(text=res)
+
+	lbl = Label(window, text='Введите общую тему в предложном падеже')
+	lbl.grid(column=0, row=0)
+
+	txt = Entry(window, width=20)
+	txt.grid(column=0, row=1)
+
+	btn = Button(window, text="Клик!", command=clicked)
+	btn.grid(column=1, row=1)
+
+	window.mainloop()
+
+
+def rad4():
+	window = Tk()
+	window.title("Другое")
+	window.geometry('400x250')
+
+	def clicked():
+		probability = random.randint(1, 4)
+		
+		if probability == 1:
+			res = "{}.\nВход в даркнет рядом.".format(txt.get())
+			lbl.configure(text=res)
+		if probability == 2:
+			res = "{}.\nЗанято!".format(txt.get())
+			lbl.configure(text=res)
+		if probability ==3:
+			res = "{}.\nОпять заразу из интернета тащит.".format(txt.get())
+			lbl.configure(text=res)
+		if probability == 4:
+			res = "{}.\nЭто не я, клянусь.".format(txt.get())
+			lbl.configure(text=res)
+
+	lbl = Label(window, text='Введите общую тему в родительном падеже')
+	lbl.grid(column=0, row=0)
+
+	txt = Entry(window, width=20)
+	txt.grid(column=0, row=1)
+
+	btn = Button(window, text="Клик!", command=clicked)
+	btn.grid(column=1, row=1)
+
+	window.mainloop()
 
 
 # Label
@@ -28,17 +151,14 @@ lbl.grid(column=0, row=0)
 
 
 #Radio Buttons
-rad1 = Radiobutton(window, text="Доклад/сочинение", value=1, command=a)
+rad1 = Radiobutton(window, text="Доклад/сочинение", value=1, command=rad1)
 rad1.grid(column=0, row=3)
-rad2 = Radiobutton(window, text="Творческая работа", value=2, command=b)
+rad2 = Radiobutton(window, text="Творческая работа", value=2, command=rad2)
 rad2.grid(column=0, row=4)
-rad3 = Radiobutton(window, text="Каталог/сборник", value=3, command=c)
+rad3 = Radiobutton(window, text="Каталог/сборник", value=3, command=rad3)
 rad3.grid(column=0, row=5)
-rad4 = Radiobutton(window, text="Другое", value=4, command=d)
+rad4 = Radiobutton(window, text="Другое", value=4, command=rad4)
 rad4.grid(column=0, row=6)
 
 
 window.mainloop()
-
-# def a():
-# 	messagebox.askquestion('Доклад/сочинение', 'Название файла')
