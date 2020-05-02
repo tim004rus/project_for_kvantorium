460515890e937d862be9e4a194bc692b6edb7872
tim004rus <63792189+tim004rus@users.noreply.github.com>
2020-05-02 14:34:45 +0500

    02.05.2020

--- project.py
+++ project.py
@@ -8,29 +8,33 @@ from tkinter.ttk import Radiobutton
 #Program name
 window = Tk()
 window.title("Названия для файлов")
-window.geometry('400x250')
+window.geometry('300x300')
+
+# Label
+lbl = Label(window, text='Выберите категорию файла:')
+lbl.grid(column=0, row=0)
 
 
 # MessageBox
 def rad1():
 	window = Tk()
 	window.title("Доклад/сочинение")
-	window.geometry('400x250')
+	window.geometry('400x80')
 
 	def clicked():
 		probability = random.randint(1, 4)
 
 		if probability == 1:
-			res = "Доклад про {}.\nТеперь и с перцем.".format(txt.get())
+			res = "Доклад про {}. Теперь и с перцем.".format(txt.get())
 			lbl.configure(text=res)
 		if probability == 2:
-			res = "Сочинение о {}.\nВозможна поломка мозгов.".format(txt.get())
+			res = "Сочинение о {}. Возможна поломка мозгов.".format(txt.get())
 			lbl.configure(text=res)
 		if probability ==3:
-			res = "Доклад о {}.\nНе открывать до 30.07.30.".format(txt.get())
+			res = "Доклад о {}. Не открывать до 30.07.30.".format(txt.get())
 			lbl.configure(text=res)
 		if probability == 4:
-			res = "Сочинение о {}.\nВсе права защищены.".format(txt.get())
+			res = "Сочинение о {}. Все права защищены.".format(txt.get())
 			lbl.configure(text=res)
 
 
@@ -41,7 +45,7 @@ def rad1():
 	txt.grid(column=0, row=1)
 
 	btn = Button(window, text="Клик!", command=clicked)
-	btn.grid(column=1, row=1)
+	btn.grid(column=0, row=2)
 
 	window.mainloop()
 
@@ -49,22 +53,22 @@ def rad1():
 def rad2():
 	window = Tk()
 	window.title("Творческая работа")
-	window.geometry('400x250')
+	window.geometry('400x80')
 
 	def clicked():
 		probability = random.randint(1, 4)
 		
 		if probability == 1:
-			res = "Творческая работа по теме {}.\nКто нашёл тот здохнет".format(txt.get())
+			res = "Творческая работа по теме {}. Кто нашёл тот здохнет".format(txt.get())
 			lbl.configure(text=res)
 		if probability == 2:
-			res = "Творческая работа по теме {}.\nС любовью из психушки.".format(txt.get())
+			res = "Творческая работа по теме {}. С любовью из психушки.".format(txt.get())
 			lbl.configure(text=res)
 		if probability ==3:
-			res = "Творческая работа по теме {}.\nДальтоникам посвящается...".format(txt.get())
+			res = "Творческая работа по теме {}. Дальтоникам посвящается...".format(txt.get())
 			lbl.configure(text=res)
 		if probability == 4:
-			res = "Творческая работа по теме {}.\nПотом сделаю, честно".format(txt.get())
+			res = "Творческая работа по теме {}. Потом сделаю, честно".format(txt.get())
 			lbl.configure(text=res)
 
 	lbl = Label(window, text='Введите общую тему в предложном падеже')
@@ -74,7 +78,7 @@ def rad2():
 	txt.grid(column=0, row=1)
 
 	btn = Button(window, text="Клик!", command=clicked)
-	btn.grid(column=1, row=1)
+	btn.grid(column=0, row=2)
 
 	window.mainloop()
 
@@ -82,22 +86,22 @@ def rad2():
 def rad3():
 	window = Tk()
 	window.title("Каталог/сборник")
-	window.geometry('400x250')
+	window.geometry('400x80')
 
 	def clicked():
 		probability = random.randint(1, 4)
 		
 		if probability == 1:
-			res = "Каталог {}.\nОн точно был где-то тут.".format(txt.get())
+			res = "Каталог {}. Он точно был где-то тут.".format(txt.get())
 			lbl.configure(text=res)
 		if probability == 2:
-			res = "Сборник {}.\nМы проверили, всё чисто.".format(txt.get())
+			res = "Сборник {}. Мы проверили, всё чисто.".format(txt.get())
 			lbl.configure(text=res)
 		if probability ==3:
-			res = "Каталог {}.\nКажется, вирусняк.".format(txt.get())
+			res = "Каталог {}. Кажется, вирусняк.".format(txt.get())
 			lbl.configure(text=res)
 		if probability == 4:
-			res = "Сборник {}.\nАбсолютно бесполезен.".format(txt.get())
+			res = "Сборник {}. Абсолютно бесполезен.".format(txt.get())
 			lbl.configure(text=res)
 
 	lbl = Label(window, text='Введите общую тему в предложном падеже')
@@ -107,7 +111,7 @@ def rad3():
 	txt.grid(column=0, row=1)
 
 	btn = Button(window, text="Клик!", command=clicked)
-	btn.grid(column=1, row=1)
+	btn.grid(column=0, row=2)
 
 	window.mainloop()
 
@@ -115,22 +119,22 @@ def rad3():
 def rad4():
 	window = Tk()
 	window.title("Другое")
-	window.geometry('400x250')
+	window.geometry('400x80')
 
 	def clicked():
 		probability = random.randint(1, 4)
 		
 		if probability == 1:
-			res = "{}.\nВход в даркнет рядом.".format(txt.get())
+			res = "{}. Вход в даркнет рядом.".format(txt.get())
 			lbl.configure(text=res)
 		if probability == 2:
-			res = "{}.\nЗанято!".format(txt.get())
+			res = "{}. Занято!".format(txt.get())
 			lbl.configure(text=res)
 		if probability ==3:
-			res = "{}.\nОпять заразу из интернета тащит.".format(txt.get())
+			res = "{}. Опять заразу из интернета тащит.".format(txt.get())
 			lbl.configure(text=res)
 		if probability == 4:
-			res = "{}.\nЭто не я, клянусь.".format(txt.get())
+			res = "{}. Это не я, клянусь.".format(txt.get())
 			lbl.configure(text=res)
 
 	lbl = Label(window, text='Введите общую тему в родительном падеже')
@@ -140,24 +144,19 @@ def rad4():
 	txt.grid(column=0, row=1)
 
 	btn = Button(window, text="Клик!", command=clicked)
-	btn.grid(column=1, row=1)
+	btn.grid(column=0, row=2)
 
 	window.mainloop()
 
 
-# Label
-lbl = Label(window, text='Выберите категорию файла:')
-lbl.grid(column=0, row=0)
-
-
 #Radio Buttons
 rad1 = Radiobutton(window, text="Доклад/сочинение", value=1, command=rad1)
-rad1.grid(column=0, row=3)
 rad2 = Radiobutton(window, text="Творческая работа", value=2, command=rad2)
-rad2.grid(column=0, row=4)
 rad3 = Radiobutton(window, text="Каталог/сборник", value=3, command=rad3)
-rad3.grid(column=0, row=5)
 rad4 = Radiobutton(window, text="Другое", value=4, command=rad4)
+rad1.grid(column=0, row=3)
+rad2.grid(column=0, row=4)
+rad3.grid(column=0, row=5)
 rad4.grid(column=0, row=6)
 
 
